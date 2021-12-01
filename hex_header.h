dbc6fd497c88e694aec19178aaa955eea462a464
Baran Solmaz <80854458+Brnslmz@users.noreply.github.com>
2021-12-02 00:09:32 +0300

    Add files via upload

--- hex_header.h
+++ hex_header.h
@@ -1,15 +1,87 @@
 #ifndef hex_header
-#define hex_header
+#define hex_header 
 
-void print_table(char table[][12] , int size);//prints the array 
-void reset_table(char table[][12]);//makes all variables '.' in array
-void play_w_friend(char table[][12] , int size);//User1 versus User2
-void play_w_computer(char table[][12] , int size);//User1 versus Computer
-int find_initial(char table[][12], int size,char player,int x,int y);//Finds the first character of players in rows and columns
-int check_input(int size ,int y,char x);//controls the inputs if it is in the range or not
-int check_location(int y,char x,char table[][12]);//checks the location if it is empty or not
-int check_table(char table[][12],int size);//checks all variables in array if there is any empty location to play
-int check_win(char table[][12], int size,char player,int x,int y);//checks the paths for players 
-void computers_turn(char table[][12], int size,int y,char x);//plays computer's moves
-void put_empty(char table[][12], int size,int y,char x);
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+enum Cell_type
+{
+	empty='.',player1='x',player2='o',computer='o'
+};
+class Hex {       
+	public: 
+		class Cell
+		{
+			public:
+				char getX()const;
+				void setX(char _x_);
+				int getY()const;
+				void setY(char _y_);
+				char getState()const;
+				void setState(char _type_);
+				Cell();
+				Cell(char _x_,int _y_);
+			private:
+				int y;
+				char x;
+				char type;
+		};
+		
+		static int totalMarked() {return marked;}
+		Hex(int _size_,int _type_);
+		explicit Hex();
+		Hex(const std::string& filename);
+		
+		Hex(const Hex &hex2);
+		~Hex();
+		const Hex& operator = (const Hex& hex2);
+
+		Hex operator--( int );
+		Hex& operator--();
+		bool operator==(Hex& hex2);
+		friend ostream &operator<<(ostream &out,const Hex &game);
+		friend ofstream &operator<<(ofstream &out,const Hex &game);
+		friend ifstream &operator>>(ifstream &in,Hex &game);
+		
+		void setSize(int _size_);
+		int getSize()const;
+		void setType(int _type_);
+		int getType()const;
+		void loadGame(const std::string& filename);
+		void saveGame(const std::string& filename);
+		void playGame();
+		const Cell play();
+		void play(Cell cell);
+		int marked_cells()const;
+		bool compare_games(const Hex& hex2);
+		const int isFinished();
+		void startGame();
+		const float userScore(char player);
+	private:
+		Cell **hexCells;
+		int neighboorCells_x(int i ,int j,char player);
+		int neighboorCells_o(int i ,int j,char player);
+		void putTable(char player,int y,int x);
+		char getTable(int y,int x)const;
+		char last_char;
+		int size,type,last_int;
+		void create_table();
+		int check_turn();
+		int options(const std::string& input);
+		int find_initial(char player,int x,int y);
+		int check_win(char player,int x,int y);
+		void make_upper(char winner);
+		int make_atoi(const std::string& input);
+		int check_input(int y,char x);
+		Cell put_empty(int y,char x);
+		Cell computers_turn(int y,char x);
+		static int marked;
+		Cell *moves;
+		void putLast(Cell cell);
+		Cell extractCell(int y,char x)const ;
+		const int whoIstheWinner();
+};	
 #endif
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 target:test
 
-test: main.cpp hex_header.cpp hex_header.h
-	g++ main.cpp hex_header.cpp 
+test: 1801042601.cpp hex_header.cpp hex_header.h
+	g++ 1801042601.cpp hex_header.cpp -std=c++11
 	./a.out
